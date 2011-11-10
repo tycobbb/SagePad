ee253c7f07c69374020a609494011fdaff1ffe15
Ty Cobb <derkis.cobb@gmail.com>
2011-11-09 18:15:43 -0600

    Added more broke shit

---
+++ SagePad/SagePad/SagePadTouchView.h
@@ -0,0 +1,13 @@
+//
+//  SagePadTouchView.h
+//  SagePad
+//
+//  Created by Matthew Cobb on 11/9/11.
+//  Copyright 2011 UIC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SagePadTouchView <NSObject>
+
+@end
